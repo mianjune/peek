a4fb701179f26c1266d3d8a35e6e222a9f28370a
Mianjune Hong <mianjune@gmail.com>
2022-04-21 10:29:07 +0800

    feat: add 'list' command in backup.sh

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -9,20 +9,27 @@ cd $base_dir
 _exec() { printf "> \e[1;37m$*\e[0m\n"; "$@"; }
 _log() { printf "\e[1;33m[`date '+%F %T'`] \e[32m$*\e[0m\n"; }
 
-_usage() { printf %b "\e[32m$0\e[0m version: v1.0 by Mianjune
+_usage() { printf %b "\e[1;32m$0\e[0m version: v1.0 by Mianjune
 A backup manager
 
-Usage: \e[32m$0 SUB_COMMAND\e[0m
+Usage: \e[1;32m$0 SUB_COMMAND\e[0m
 
 Commands:
-    \e[1;32mschedule\e[0m          update backup data and commit to repository
-    \e[1;32menable  SCRIPT\e[0m    enable the udpating script
-    \e[1;32mdisable SCRIPT\e[0m    disable the udpating script
+    \e[1;32mlist\e[0m		list all updating scripts
+    \e[1;32mschedule\e[0m		update backup data and commit to repository
+    \e[1;32menable  SCRIPT\e[0m	enable the updating script
+    \e[1;32mdisable SCRIPT\e[0m	the updating script
 
 "; }
 
 
-_schedule() {
+_list() {
+  _e=$'\e['
+  ls $bin_dir|grep .sh|sed -r "s/^(.+)\.off/${_e}1;36m# \1/;tr;s/^/${_e}1;32m  /;:r;s \.sh  ;s \$ ${_e}0m "
+}
+
+
+_schedule() { # args: name
   _log schedule
   # refresh backup data
   for f in $bin_dir/*.sh; do
@@ -77,8 +84,9 @@ _disable() { # arg1: script_path
 
 
 case $1 in
-    schedule) _schedule;;
-    enable)   _enable "${@:2}";;
-    disable)  _disable "${@:2}";;
-    *)        _usage; exit 1;
+  ls|list)	_list;;
+  schedule)	_schedule;;
+  enable)	_enable "${@:2}";;
+  disable)	_disable "${@:2}";;
+  *)		_usage; exit 1;
 esac
